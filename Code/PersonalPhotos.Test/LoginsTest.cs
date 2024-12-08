721b7e19eedaba7fa288384614a85413148d11a6
cagdasscetin <cagdasscetin@gmail.com>
2024-12-08 21:28:22 +0300

    Testing when ModelState is invalid

--- Code/PersonalPhotos.Test/LoginsTest.cs
+++ Code/PersonalPhotos.Test/LoginsTest.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using PersonalPhotos.Controllers;
+using PersonalPhotos.Models;
 
 namespace PersonalPhotos.Test;
 
@@ -28,5 +29,15 @@ public class LoginsTest
         Assert.Equal("Login", result.ViewName, ignoreCase: true);
         // Assert.IsAssignableFrom<IActionResult>(result);
         // Assert.IsType<ViewResult>(result);
+    }
+
+    [Fact]
+    public async Task Login_GivenModelStateInvalid_ReturnLoginView()
+    {
+        _controller.ModelState.AddModelError("Test", "Test");
+        
+        var result = await _controller.Login(Mock.Of<LoginViewModel>()) as ViewResult;
+        
+        Assert.Equal("Login", result.ViewName, ignoreCase: true);
     }
 }
