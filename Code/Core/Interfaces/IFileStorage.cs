5d9f3f22f38480cc63aa49837822ddde64144107
cagdasscetin <cagdasscetin@gmail.com>
2024-12-08 21:23:01 +0300

    Project initialized

---
+++ Code/Core/Interfaces/IFileStorage.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http ;
+
+namespace Core.Interfaces;
+
+public interface IFileStorage
+{
+    Task StoreFile(Microsoft.AspNetCore.Http.FormFile file, string key);
+}
---
+++ Code/Core/Services/LocalFileStorage.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Core.Services;
+
+public class LocalFileStorage : IFileStorage
+{
+    private readonly IWebHostEnvironment _env;
+
+    public LocalFileStorage(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+
+    public async Task StoreFile(FormFile file, string key)
+    {
+        const string rootPath = "PhotoStore";
+
+        var wwwRootFolder = _env.WebRootPath;
+        var userFolder = Path.Combine(wwwRootFolder, rootPath);
+        var folder = Path.Combine(userFolder, key);
+
+        if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
+
+        var fullFilePath = Path.Combine(folder, Path.GetFileName(file.FileName) ?? string.Empty);
+
+        using var targetStream = new FileStream(fullFilePath, FileMode.Create);
+        await file.CopyToAsync(targetStream);
+        targetStream.Close();
+    }
+}
